c8b672b0068f7773418d86f484ccf32a459b1954
Raphael Mitsch <laarem33@hotmail.com>
2018-02-08 19:01:18 +0100

    Started work on parallel coordinates for categorical variables.

---
+++ source/frontend/static/js/charts/ParallelCoordinates.js
@@ -0,0 +1,10 @@
+import Chart from "./Chart.js"
+
+/**
+ * Parallel coordinates plot.
+ * Utilizes https://github.com/syntagmatic/parallel-coordinates.
+ */
+export default class ParallelCoordinates extends Chart
+{
+
+}
