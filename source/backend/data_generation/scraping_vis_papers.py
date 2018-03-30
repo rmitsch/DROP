3d54d8d748b45ccb4a2dade243e9f44e59fb09ef
Raphael Mitsch <laarem33@hotmail.com>
2018-03-30 14:45:25 +0200

    Fixing errors for #19.

--- source/backend/data_generation/scraping_vis_papers.py
+++ source/backend/data_generation/scraping_vis_papers.py
@@ -6,8 +6,7 @@ import scrapely
 import html2text
 from urllib.parse import urlencode
 from urllib.request import Request, urlopen
-import requests
-from bs4 import BeautifulSoup
+import csv
 
 ########################################
 # !. Get cluster information from
@@ -42,65 +41,96 @@ data = {
 }
 paper_scraper.train(url, data)
 
-
 # Loop through clusters, aggregate information.
-for i, cluster_title in enumerate(parsed_content_list):
-    if cluster_title != "":
-        clean_cluster_title = cluster_title.strip()
+# Stopped last time
+with open('vis_papers.csv', 'a') as output_file:
+    # Initialize files.
+    dict_writer = csv.DictWriter(output_file, ["cluster_title", "conference", "year", "title", "abstract", "url"])
+    dict_writer.writeheader()
+    error_file = open('vis_paper_erros.txt', 'a')
 
-        # Get detailled paper information.
-        response = urlopen(
-            Request(
-                'http://keyvis.org/load3.php',
-                urlencode({'expert': str(i)}).encode()
-            )
-        ).read().decode()
-        parsed_paper_response = h.handle(response)
+    # Note: Stopped last time with cluster .
+    for i, cluster_title in enumerate(parsed_content_list):
+        if cluster_title != "":
+            # Remove trailing and leading whitespaces.
+            clean_cluster_title = cluster_title.strip()
 
-        # Dismiss keywords at beginning, only keep from first paper on.
-        parsed_paper_response = parsed_paper_response[
-            parsed_paper_response.index("---|---|---") + 11:
-        ]
+            # Print progress.
+            print(str(i / float(len(parsed_content_list)) * 100) + "% | " + clean_cluster_title)
 
-        # Split by ---|---|--- to get information on individual papers.
-        parsed_paper_list = parsed_paper_response.strip().split("---|---")
+            # Get detailled paper information.
+            response = urlopen(
+                Request(
+                    'http://keyvis.org/load3.php',
+                    urlencode({'expert': str(i)}).encode()
+                )
+            ).read().decode()
+            parsed_paper_response = h.handle(response)
 
-        # Loop through papers, collect information on papers.
-        for paper in parsed_paper_list:
-            paper_info = {
-                "cluster_title": clean_cluster_title,
-                "conference": None,
-                "year": None,
-                "title": None,
-                "abstract": None,
-                "url": None
-            }
+            # Dismiss keywords at beginning, only keep from first paper on.
+            parsed_paper_response = parsed_paper_response[
+                parsed_paper_response.index("---|---|---") + 11:
+            ]
 
-            # Split paper in description to extract conference, year and link.
-            paper_parts = paper.split("|")
+            # Split by ---|---|--- to get information on individual papers.
+            parsed_paper_list = parsed_paper_response.strip().split("---|---")
 
-            if len(paper_parts) > 1:
-                paper_info["conference"] = paper_parts[0].strip()
-                paper_info["year"] = paper_parts[1].strip()
+            # Loop through papers, collect information on papers.
+            for paper in parsed_paper_list:
+                paper_info = {
+                    "cluster_title": clean_cluster_title,
+                    "conference": None,
+                    "year": None,
+                    "title": None,
+                    "abstract": None,
+                    "url": None
+                }
 
-                # For URL to paper: Find last occurence of round bracket, read URL except closing round bracket).
-                paper_info["url"] = paper_parts[2][paper_parts[2].rfind('(') + 1:-2]
-                content = paper_scraper.scrape(paper_info["url"])[0]
-                # Get paper title.
-                paper_info["title"] = h.handle(content["title"][0]) \
-                    .replace('\n', ' ') \
-                    .replace('\r', '') \
-                    .replace(" - IEEE Journals & Magazine", "") \
-                    .strip()
-                # Get abstract.
-                abstract = h.handle(content["abstract"][0]) \
-                    .replace('\n', ' ') \
-                    .replace('\r', '') \
-                    .strip()
-                abstract_index = abstract.index('"abstract":') + 12
-                abstract_end_index = abstract_index + (abstract[abstract_index + 1:].index('","')) + 1
-                paper_info["abstract"] = abstract[abstract_index:abstract_end_index]
+                # Split paper in description to extract conference, year and link.
+                paper_parts = paper.split("|")
 
-            if paper_info["abstract"] is not None:
-                print(paper_info)
-        exit()
+                try:
+                    if len(paper_parts) > 1:
+                        paper_info["conference"] = paper_parts[0].strip()
+                        paper_info["year"] = paper_parts[1].strip()
+
+                        # For URL to paper: Find last occurence of round bracket, read URL except closing round
+                        # bracket).
+                        paper_info["url"] = paper_parts[2][paper_parts[2].rfind('(') + 1:-2]
+                        content = paper_scraper.scrape(paper_info["url"])[0]
+                        # Get paper title.
+                        paper_info["title"] = h.handle(content["title"][0]) \
+                            .replace('\n', ' ') \
+                            .replace('\r', '') \
+                            .replace(" - IEEE Journals & Magazine", "") \
+                            .strip()
+                        # Get abstract.
+                        abstract = h.handle(content["abstract"][0]) \
+                            .replace('\n', ' ') \
+                            .replace('\r', '') \
+                            .strip()
+                        abstract_index = abstract.index('"abstract":') + 12
+                        abstract_end_index = abstract_index + (abstract[abstract_index + 1:].index('","')) + 1
+                        paper_info["abstract"] = abstract[abstract_index:abstract_end_index]
+
+                    if paper_info["abstract"] is not None:
+                        # Dump paper info list to CSV.
+                        dict_writer.writerow(paper_info)
+                        print("    " + str(paper_info))
+
+                except Exception as e:
+                    print("*************")
+                    print("Error: " + paper_info["url"])
+                    error_file.write(paper_info["url"] + "\n")
+                    print("*************")
+
+    error_file.close()
+
+# Open error file, try with alternative scraper.
+paper_scraper_visual = scrapely.Scraper()
+url = "https://www.computer.org/csdl/proceedings/visual/2000/6478/00/00885702-abs.html"
+data = {
+    'title': 'Image based rendering with stable frame rates',
+    'abstract': 'Presents an efficient keyframeless image-based rendering technique. An intermediate image is used to exploit the coherences among neighboring frames. The pixels in the intermediate image are first rendered by a ray-casting method and then warped to the intermediate image at the current viewpoint and view direction. We use an offset buffer to record the precise positions of these pixels in the intermediate image. Every frame is generated in three steps: warping the intermediate image onto the frame, filling in holes, and selectively rendering a group of "old" pixels. By dynamically adjusting the number of those "old" pixels in the last step, the workload at every frame can be balanced. The pixels generated by the last two steps make contributions to the new intermediate image. Unlike occasional keyframes in conventional image-based rendering, which need to be totally re-rendered, intermediate images only need to be partially updated at every frame. In this way, we guarantee more stable frame rates and more uniform image qualities. The intermediate image can be warped efficiently by a modified incremental 3D warp algorithm. As a specific application, we demonstrate our technique with a voxel-based terrain rendering system.'
+}
+paper_scraper_visual.train(url, data)
