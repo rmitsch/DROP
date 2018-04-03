f408cd141e7f753a8dd84c3710d914227d97fb88
Raphael Mitsch <laarem33@hotmail.com>
2018-04-03 18:03:54 +0200

    Added fastText setup.

--- setup/setup.sh
+++ setup/setup.sh
@@ -8,3 +8,8 @@ python -c 'from sklearn.datasets import fetch_mldata; fetch_mldata("MNIST origin
 # Download repository and install package for multi-threaded t-SNE.
 pip install --no-cache-dir git+https://github.com/DmitryUlyanov/Multicore-TSNE.git
 pip install --no-cache-dir git+https://github.com/samueljackson92/coranking.git
+
+# Install fastText with Python bindings for classification of VIS documents.
+git clone https://github.com/facebookresearch/fastText.git
+cd fastText
+python setup.py install
