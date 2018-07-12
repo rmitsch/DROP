756cb23c9f74f8ab5b14e03324cf0419defb7016
Raphael Mitsch <laarem33@hotmail.com>
2018-07-12 13:51:23 +0200

    #32: Fixed heatmap binning issue.

--- source/frontend/static/js/data/DissonanceDataset.js
+++ source/frontend/static/js/data/DissonanceDataset.js
@@ -25,24 +25,29 @@ export default class DissonanceDataset extends Dataset
         this._supportedDRModelMeasure   = supportedDRModelMeasure;
 
         // Add DR model measure to records.
-        this._complementDatasetWithDRMeasure();
+        this._complementModelMeasures();
 
         // Set up containers for crossfilter data.
         this._crossfilter = crossfilter(this._data);
 
         // Initialize crossfilter data.
         this._initSingularDimensionsAndGroups();
+        this._initializeBins();
         this._initHistogramDimensionsAndGroups();
         this._initBinaryDimensionsAndGroups();
     }
 
     /**
-     * Adds model measures to internal dataset.
+     * Complements records with the corresponding DR model's measure.
      * @private
      */
-    _complementDatasetWithDRMeasure()
+    _complementModelMeasures()
     {
         let drModelIDToMeasureValue = {};
+
+        // ----------------------------------------------------
+        // 1. Add DR model measures to samples-in-models.
+        // ----------------------------------------------------
 
         // Iterate over DR model indices, store corresponding measure.
         for (let drModelID in this._drModelMetadata._dataIndicesByID) {
@@ -56,7 +61,34 @@ export default class DissonanceDataset extends Dataset
                 this._data[sampleInModelIndex].model_id
             ];
         }
+    }
+    /**
+     * Adds one dummy record per heatmap bin to make sure heatmap doesn't omit any rows/columns.
+     * @private
+     */
+    _initializeBins()
+    {
+        // ----------------------------------------------------
+        // Add n * m dummy records to make sure all bins exist.
+        // ----------------------------------------------------
 
+        let rowAttribute    = this._supportedDRModelMeasure;
+        let colAttribute    = "measure";
+        let rowExtrema      = this._cf_extrema[rowAttribute];
+        let colExtrema      = this._cf_extrema[colAttribute];
+        let rowBinWidth     = (rowExtrema.max - rowExtrema.min) / this._binCounts.y;
+        let colBinWidth     = (colExtrema.max - colExtrema.min) / this._binCounts.x;
+
+        for (let i = 0; i < this._binCounts.y; i ++) {
+            for (let j = 0; j < this._binCounts.x; j ++) {
+                let dummyRecord = {model_id: -1, sample_id: -1};
+                dummyRecord[colAttribute] = j * colBinWidth;
+                dummyRecord[rowAttribute] = i * rowBinWidth;
+
+                // Add record to crossfilter dataset.
+                this._crossfilter.add([dummyRecord]);
+            }
+        }
     }
 
     _initSingularDimensionsAndGroups()
@@ -108,44 +140,51 @@ export default class DissonanceDataset extends Dataset
                     colValue = colExtrema.max - colBinWidth;
                 }
 
+                let binnedColValue  = colValue / colBinWidth;
+                let binnedRowValue  = rowValue / rowBinWidth;
+
                 // Calculate bin index and use as column/row number.
-                return [+Math.floor(colValue / colBinWidth), +Math.floor(rowValue / rowBinWidth)];
+                // Use round() instead of floor, if record is a dummy (to ensure proper rounding for bin placmeent).
+                return [
+                    d.model_id !== -1 ? +Math.floor(binnedColValue) : +Math.round(binnedColValue),
+                    d.model_id !== -1 ? +Math.floor(binnedRowValue) : +Math.round(binnedRowValue)
+                ];
             }
         );
 
         // 2. Define group as sum of intersection sample_id/model_id - since only one element per
         // group exists, sum works just fine.
         this._cf_groups[attribute] = this._cf_dimensions[attribute].group().reduceCount();
-
-         let sortedData          = JSON.parse(JSON.stringify(this._cf_groups[attribute].all()))
-
-        // Sort data by number of entries in this attribute's histogram.
-        sortedData.sort(function(entryA, entryB) {
-            if (entryA.key[0] > entryB.key[0])
-                return 1;
-            else if (entryA.key[0] < entryB.key[0])
-                return -1;
-            else {
-                if (entryA.key[1] > entryB.key[1])
-                    return 1;
-                else if (entryA.key[1] < entryB.key[1])
-                    return -1;
-                else
-                    return 0;
-            }
-        });
-
-        // CONTINUE HERE:
-        //     - how to size / adjust heatmap properly if rows/columns are missing?
-        //       -> why does heatmap not leave a free col for col 1, since no data is available?
-
-         for (let bla of sortedData)
-             console.log(bla.key + " -> " + bla.value);
-
-        var data = this._cf_groups[attribute].all();
-        var ncols = d3.set(data.map(function(x) { return x.key[0]; })).size();
-        var nrows = d3.set(data.map(function(x) { return x.key[1]; })).size();
-        console.log(ncols + "; " + nrows);
+        //
+        //  let sortedData          = JSON.parse(JSON.stringify(this._cf_groups[attribute].all()))
+        //
+        // // Sort data by number of entries in this attribute's histogram.
+        // sortedData.sort(function(entryA, entryB) {
+        //     if (entryA.key[0] > entryB.key[0])
+        //         return 1;
+        //     else if (entryA.key[0] < entryB.key[0])
+        //         return -1;
+        //     else {
+        //         if (entryA.key[1] > entryB.key[1])
+        //             return 1;
+        //         else if (entryA.key[1] < entryB.key[1])
+        //             return -1;
+        //         else
+        //             return 0;
+        //     }
+        // });
+        //
+        // // CONTINUE HERE:
+        // //     - how to size / adjust heatmap properly if rows/columns are missing?
+        // //       -> why does heatmap not leave a free col for col 1, since no data is available?
+        //
+        //  for (let bla of sortedData)
+        //      console.log(bla.key + " -> " + bla.value);
+        //
+        // var data = this._cf_groups[attribute].all();
+        // var ncols = d3.set(data.map(function(x) { return x.key[0]; })).size();
+        // var nrows = d3.set(data.map(function(x) { return x.key[1]; })).size();
+        // console.log(ncols + "; " + nrows);
 
         // 3. Find extrema.
         this._calculateHistogramExtremaForAttribute(attribute);
