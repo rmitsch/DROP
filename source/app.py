2a6161d23b3f97346a791ebdc344c5fae6968157
Raphael Mitsch <laarem33@hotmail.com>
2018-08-17 13:18:13 +0200

    Pushed version number for fix.

--- source/app.py
+++ source/app.py
@@ -27,7 +27,7 @@ def init_flask_app():
     )
 
     # Define version.
-    flask_app.config["VERSION"] = "0.5.4"
+    flask_app.config["VERSION"] = "0.5.5"
 
     # Store metadata template. Is assembled once in /get_metadata.
     flask_app.config["METADATA_TEMPLATE"] = None
