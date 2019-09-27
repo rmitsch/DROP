7d88ad8ed22e194b6256daeea540f491ffa51726
Raphael Mitsch <laarem33@hotmail.com>
2019-09-27 15:43:43 +0200

    Bumped version.

--- source/utils.py
+++ source/utils.py
@@ -230,7 +230,7 @@ class Utils:
         )
 
         # Define version.
-        flask_app.config["VERSION"] = "0.18.1"
+        flask_app.config["VERSION"] = "0.19.0"
 
         # Store metadata template. Is assembled once in /get_metadata.
         flask_app.config["METADATA_TEMPLATE"] = None
