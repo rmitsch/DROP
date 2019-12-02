1bbf88a21cba3c55738b5399cf4e6d94b72c8bea
Raphael Mitsch <laarem33@hotmail.com>
2019-12-02 18:01:22 +0100

    Added script for testing vis. of displaced neighbour's connections.

--- source/pairwise_displacement_connection_test.py
+++ source/pairwise_displacement_connection_test.py
@@ -17,14 +17,29 @@ pairwise_displacement_data: pd.DataFrame = pd.read_pickle(
     columns={0: "neighbour_0", 1: "neighbour_1"}
 )
 
+max_cell_idx: int = pairwise_displacement_data.high_dim_neighbour_rank.max()
+bin_interval: int = int(max_cell_idx / 15)
+for hd_i, hd_idx in enumerate(range(0, max_cell_idx, bin_interval)):
+    print(hd_i)
+    hd_cell_idxs: set = set(range(hd_idx, hd_idx + bin_interval))
 
-with pd.option_context('display.max_rows', None, 'display.max_columns', None, 'display.width', None):
-    print(pairwise_displacement_data.head())
-    print(original_dataset.head())
+    for ld_i, ld_idx in enumerate(range(0, max_cell_idx, bin_interval)):
+        ld_cell_idxs: set = set(range(ld_idx, ld_idx + bin_interval))
 
-fig = px.scatter(x=original_dataset[0], y=original_dataset[1])
-# todo
-#  - evaluate data, pick data fitting "cell".
-#  - add line traces for each pair of records in data selected in previous step.
-# fig.show()
+        relevant_connections: pd.DataFrame = pairwise_displacement_data[
+            pairwise_displacement_data.high_dim_neighbour_rank.isin(hd_cell_idxs) &
+            pairwise_displacement_data.low_dim_neighbour_rank.isin(ld_cell_idxs)
+        ]
 
+        fig = px.scatter(x=original_dataset[0], y=original_dataset[1])
+        for idx, record in relevant_connections.iterrows():
+            fig.add_trace(
+                go.Scatter(
+                    x=[record.source_0, record.neighbour_0],
+                    y=[record.source_1, record.neighbour_1],
+                    mode='lines',
+                    name='lines'
+                )
+            )
+        fig.update_layout(title=str(hd_i) + " : " + str(ld_i) + "(" + str(len(relevant_connections)) + ")")
+        fig.write_image("/home/raphael/Development/data/DROP/corm-demo-images/" + str(hd_i) + "_" + str(ld_i) + ".png")
