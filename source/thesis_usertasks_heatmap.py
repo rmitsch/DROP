f8fde794dbc116a5c8d63f04815d66b40f681df4
Raphael Mitsch <laarem33@hotmail.com>
2020-02-24 17:17:59 +0100

    Finished script for user task-to-component heatmap.

--- source/thesis_usertasks_heatmap.py
+++ source/thesis_usertasks_heatmap.py
@@ -1,76 +1,91 @@
 import pandas as pd
 import itertools
 import seaborn as sns
+import matplotlib.pyplot as plt
 
 
-components: list = ["C" + str(i) for i in range(1, 12)]
+components: list = ["C" + str(i).zfill(2) for i in range(1, 14)]
 tasks: list = [
-    *["T1." + str(i) for i in range(1, 10)],
+    *["T1." + str(i) for i in range(1, 9)],
     *["T2." + str(i) for i in range(1, 5)],
     *["T3." + str(i) for i in range(1, 4)]
 ]
 matrix: pd.DataFrame = pd.DataFrame(list(itertools.product(components, tasks)), columns=["Component", "Task"])
 matrix["interaction"] = 0
 matrix.loc[
-    (matrix.Component == "C1") &
-    (matrix.Task.isin(("T1.3", "T2.1", "T2.3", "T3.3"))),
+    (matrix.Component == "C01") &
+    (matrix.Task.isin(("T1.2", "T2.1", "T2.3", "T3.3"))),
     "interaction"
 ] = 1
 matrix.loc[
-    (matrix.Component == "C2") &
-    (matrix.Task.isin(("T1.3", "T2.1", "T2.3", "T3.1", "T3.3"))),
+    (matrix.Component == "C02") &
+    (matrix.Task.isin(("T1.2", "T2.1", "T2.3", "T3.1", "T3.3"))),
     "interaction"
 ] = 1
 matrix.loc[
-    (matrix.Component == "C3") &
-    (matrix.Task.isin(("T1.3", "T2.1", "T2.3", "T3.1", "T3.3"))),
+    (matrix.Component == "C03") &
+    (matrix.Task.isin(("T1.2", "T2.1", "T2.3", "T3.1", "T3.3"))),
     "interaction"
 ] = 1
 matrix.loc[
-    (matrix.Component == "C4") &
-    (matrix.Task.isin(("T1.1", "T1.3", "T3.3"))),
+    (matrix.Component == "C04") &
+    (matrix.Task.isin(("T1.1", "T1.2", "T3.3"))),
     "interaction"
 ] = 1
 matrix.loc[
-    (matrix.Component == "C5") &
-    (matrix.Task.isin(("T2.2", "T2.3"))),
+    (matrix.Component == "C05") &
+    (matrix.Task.isin(("T2.2", "T2.3", "T2.4"))),
     "interaction"
 ] = 1
 matrix.loc[
-    (matrix.Component == "C6") &
+    (matrix.Component == "C06") &
     (matrix.Task.isin(("T3.2",))),
     "interaction"
 ] = 1
 
 matrix.loc[
-    (matrix.Component == "C7") &
-    (matrix.Task.isin(("T1.3",))),
+    (matrix.Component == "C07") &
+    (matrix.Task.isin(("T1.2",))),
     "interaction"
 ] = 1
 matrix.loc[
-    (matrix.Component == "C8") &
-    (matrix.Task.isin(("T1.8",))),
+    (matrix.Component == "C08") &
+    (matrix.Task.isin(("T1.7",))),
     "interaction"
 ] = 1
 matrix.loc[
-    (matrix.Component == "C9") &
-    (matrix.Task.isin(("T1.2", "T1.6", "T1.7"))),
+    (matrix.Component == "C09") &
+    (matrix.Task.isin(("T1.5", "T1.6"))),
     "interaction"
 ] = 1
 matrix.loc[
     (matrix.Component == "C10") &
-    (matrix.Task.isin(("T1.4",))),
+    (matrix.Task.isin(("T1.4", "T1.5", "T1.6"))),
     "interaction"
 ] = 1
 matrix.loc[
     (matrix.Component == "C11") &
-    (matrix.Task.isin(("T1.4",))),
+    (matrix.Task.isin(("T1.3",))),
     "interaction"
 ] = 1
 matrix.loc[
     (matrix.Component == "C12") &
-    (matrix.Task.isin(("T1.9",))),
+    (matrix.Task.isin(("T1.3",))),
+    "interaction"
+] = 1
+matrix.loc[
+    (matrix.Component == "C13") &
+    (matrix.Task.isin(("T1.8",))),
     "interaction"
 ] = 1
 
-print(matrix.head())
+# Reshape for seaborn.heatmap().
+matrix = matrix.pivot(columns="Component", index="Task", values="interaction")
+matrix = matrix.rename(columns={col: col.replace("C0", "C") for col in matrix.columns})
+ax = sns.heatmap(
+    matrix, annot=False, fmt="g", linewidths=1, cmap=sns.cubehelix_palette(8, start=0, dark=0, light=.95), cbar=False
+)
+ax.set_xlabel("Task", fontsize=20, labelpad=20)
+ax.set_ylabel("Component", fontsize=20, labelpad=20)
+ax.tick_params(labelsize=15)
+plt.show()
